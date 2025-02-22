2fcae390a00d1e07260d6c03abd4f2f7f5ff3bac
Jirka Mayer <jirkamayer97@gmail.com>
2025-02-23 00:43:42 +0100

    Conditioned react-scan into development environment only

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,19 @@
-// must be imported before React and React DOM
-// https://github.com/aidenybai/react-scan
-import { scan } from "react-scan";
-
-import { createRoot } from "react-dom/client";
-import { Application } from "./Application";
-
 /**
  * Starts up the entire application
  */
 async function bootstrapApplication() {
+  
+  // Debugging tool that displays react re-renders of components.
+  // Must be imported before React and React DOM.
+  // https://github.com/aidenybai/react-scan
+  if (process.env.NODE_ENV === "development") {
+    await import("react-scan");
+  }
+
+  // import dependencies
+  const { createRoot } = await import("react-dom/client")
+  const { Application } = await import("./Application")
+  
   // you can await async initialization code here
   // ...
 
