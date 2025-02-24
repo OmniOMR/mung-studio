fb72c9ac7f59f5a4a49c173562bf15f07b64edc8
Jirka Mayer <jirkamayer97@gmail.com>
2025-02-24 23:43:34 +0100

    Custom zoom&pan controls (inkscape inspired) and playing with screen-space styling

---
+++ src/ui/explorer/NodeEditor.tsx
@@ -0,0 +1,73 @@
+import { useAtom } from "jotai";
+import { SelectedNodeStore } from "./state/SelectedNodeStore";
+import { MouseEvent, useState } from "react";
+
+export interface NodeEditorProps {
+  readonly selectedNodeStore: SelectedNodeStore;
+}
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+const HANDLE_SIZE = 15
+
+export function NodeEditor(props: NodeEditorProps) {
+  const [node, setNode] = useAtom(props.selectedNodeStore.selectedNodeAtom);
+
+  const [mouseDownPosition, setMouseDownPosition]
+    = useState<Position | null>(null);
+
+  function handleMouseDown(e: MouseEvent) {
+    setMouseDownPosition({
+      x: e.screenX,
+      y: e.screenY,
+    })
+  }
+
+  function handleMouseUp(e: MouseEvent) {
+    setMouseDownPosition(null);
+  }
+
+  function handleMouseMove(e: MouseEvent) {
+    if (mouseDownPosition === null) return;
+    
+    const widthDelta = e.screenX - mouseDownPosition.x;
+    console.log(widthDelta);
+  }
+
+  if (node === null) {
+    return null;
+  }
+
+  const bottom = node.top + node.height;
+  const right = node.left + node.width;
+
+  return (
+    <>
+      <g
+        transform={`translate(${right}, ${bottom})`}
+        style={{
+          cursor: "nwse-resize"
+        }}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      >
+        <rect
+          x={-HANDLE_SIZE / 2}
+          y={-HANDLE_SIZE / 2}
+          width={HANDLE_SIZE}
+          height={HANDLE_SIZE}
+          strokeWidth="1"
+          stroke="black"
+          fill="rgba(255, 255, 255, 0.8)"
+          style={{
+            transform: "scale(var(--surface-screen-pixel))"
+          }}
+          />
+      </g>
+    </>
+  );
+}
--- src/ui/explorer/Surface.tsx
+++ src/ui/explorer/Surface.tsx
@@ -6,12 +6,67 @@ import { ClassVisibilityStore } from "./state/ClassVisibilityStore";
 import { NotationGraphStore } from "./state/NotationGraphStore";
 import { useAtomValue } from "jotai";
 import { SvgEdgeNode } from "./SvgEdgeNode";
+import { NodeEditor } from "./NodeEditor";
 
 export interface SurfaceProps {
   readonly backgroundImageUrl: string | null;
   readonly notationGraphStore: NotationGraphStore;
   readonly selectedNodeStore: SelectedNodeStore;
   readonly classVisibilityStore: ClassVisibilityStore;
+}
+
+function customizeD3ZoomBehaviour(
+  svgElement: d3.Selection<SVGSVGElement, unknown, null, undefined>,
+  zoom: d3.ZoomBehavior<Element, unknown>
+) {
+  // disable double-click zooming
+  svgElement.on("dblclick.zoom", null);
+
+  // mouse dragging will be done with the middle mouse button
+  // or with the primary button while holding ctrl
+  zoom.filter((event: MouseEvent) => {
+    return event.type === 'wheel' || event.type.startsWith("touch") || (
+      event.ctrlKey ? event.button == 0 : event.button == 1
+    );
+  });
+
+  // require CTRL key be pressed for wheel zooming
+  const originalD3WheelZoomHandler = svgElement.on("wheel.zoom");
+  svgElement.on("wheel.zoom", function (event: WheelEvent) {
+    if (event.ctrlKey) {
+      originalD3WheelZoomHandler?.call(this, event);
+    }
+  });
+
+  // disable the fast zoom with the CTRL key
+  // taken from the source code:
+  // https://github.com/d3/d3-zoom/blob/main/src/zoom.js#L34
+  zoom.wheelDelta(function (event: WheelEvent) {
+    return -event.deltaY * (event.deltaMode === 1 ? 0.05 : event.deltaMode ? 1 : 0.002);
+  });
+
+  // wheel panning
+  function panningDeltaScale(event: WheelEvent) {
+    return event.deltaMode === 1 ? 25 : event.deltaMode ? 500 : 1;
+  }
+  svgElement.on("wheel.custom-pan", function (event: WheelEvent) {
+    if (event.ctrlKey) return;
+    const transform = svgElement.property("__zoom") as d3.ZoomTransform;
+    const scale = (1 / transform.k) * panningDeltaScale(event);
+    if (event.shiftKey) {
+      zoom.translateBy(
+        svgElement,
+        -(event.deltaY + event.deltaX) * scale,
+        0
+      );
+    } else {
+      zoom.translateBy(
+        svgElement,
+        -event.deltaX * scale,
+        -event.deltaY * scale
+      );
+    }
+  });
 }
 
 export function Surface(props: SurfaceProps) {
@@ -27,13 +82,18 @@ export function Surface(props: SurfaceProps) {
 
     const g = svgElement.select("g");
 
-    const zoom = d3.zoom().scaleExtent([0.1, 10]).on("zoom", zoomed);
+    const zoom = d3.zoom()
+      .scaleExtent([0.1, 10])
+      .on("zoom", zoomed);
 
     svgElement.call(zoom);
 
+    customizeD3ZoomBehaviour(svgElement, zoom);
+
     function zoomed(event) {
-      const { transform } = event;
-      g.attr("transform", transform);
+      const { transform } = event as d3.D3ZoomEvent<any, any>;
+      g.attr("transform", transform.toString());
+      g.style("--surface-screen-pixel", 1.0 / transform.k);
     }
   }, []);
 
@@ -86,6 +146,7 @@ export function Surface(props: SurfaceProps) {
             notationGraphStore={props.notationGraphStore}
           />
         ))}
+        <NodeEditor selectedNodeStore={props.selectedNodeStore} />
       </g>
     </svg>
   );
