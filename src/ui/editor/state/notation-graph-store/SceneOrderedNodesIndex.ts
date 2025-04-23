e3fdf982a566a820f58df12a38aa5f33b75e2d76
Jirka Mayer <jirkamayer97@gmail.com>
2025-04-23 10:11:06 +0200

    Better performance SceneOrderedNodesIndex

--- src/ui/editor/state/notation-graph-store/SceneOrderedNodesIndex.ts
+++ src/ui/editor/state/notation-graph-store/SceneOrderedNodesIndex.ts
@@ -1,7 +1,7 @@
 import { atom, getDefaultStore } from "jotai";
 import { Node } from "../../../../mung/Node";
 import { SignalAtomWrapper } from "../SignalAtomWrapper";
-import { NodeCollection } from "./NodeCollection";
+import { NodeCollection, NodeUpdateMetadata } from "./NodeCollection";
 import { JotaiStore } from "../JotaiStore";
 import { classNameZIndex } from "../../../../mung/classNameZIndex";
 
@@ -32,6 +32,10 @@ export class SceneOrderedNodesIndex {
   private nodeCollection: NodeCollection;
 
   private signalAtom = new SignalAtomWrapper();
+
+  // the primary datastructure behind the index,
+  // mutable and never exposed. Insert-sort into this structure.
+  private _orderedMutableNodes: Node[] = [];
 
   private _nodes: readonly Node[];
   private _nodeIds: readonly number[];
@@ -73,25 +77,54 @@ export class SceneOrderedNodesIndex {
     this.jotaiStore = jotaiStore || getDefaultStore();
     this.nodeCollection = nodeCollection;
 
-    nodeCollection.onNodeInserted.subscribe(this.rebuildIndex.bind(this));
+    nodeCollection.onNodeInserted.subscribe(this.onNodeInserted.bind(this));
     nodeCollection.onNodeUpdatedOrLinked.subscribe(
-      this.rebuildIndex.bind(this),
+      this.onNodeUpdatedOrLinked.bind(this),
     );
-    nodeCollection.onNodeRemoved.subscribe(this.rebuildIndex.bind(this));
+    nodeCollection.onNodeRemoved.subscribe(this.onNodeRemoved.bind(this));
   }
 
-  private rebuildIndex() {
-    // compute the new list
-    const nodes = [...this.nodeCollection.getAllNodes()];
-    nodes.sort(nodeComparator);
-    // TODO: this sorting is slow during page load, as many nodes are inserted.
-    // This class should be updated to have a mutable array in its core from
-    // which snapshot copies are being made. Then insertion sort should be
-    // used to modify the underlying array.
+  private onNodeInserted(node: Node) {
+    this._orderedMutableNodes.push(node);
+    this._orderedMutableNodes.sort();
+    this.rebuildSecondaryStructures();
+  }
 
-    // update state
-    this._nodes = nodes;
-    this._nodeIds = nodes.map((node) => node.id);
+  private onNodeUpdatedOrLinked(e: NodeUpdateMetadata) {
+    for (let i = 0; i < this._orderedMutableNodes.length; i++) {
+      if (this._orderedMutableNodes[i].id !== e.nodeId) continue;
+
+      this._orderedMutableNodes[i] = e.newValue;
+      if (!e.isLinkUpdate) {
+        this._orderedMutableNodes.sort();
+      }
+      this.rebuildSecondaryStructures();
+      return;
+    }
+    throw new Error("Node was updated that is not present in the index.");
+  }
+
+  private onNodeRemoved(node: Node) {
+    let index = -1;
+    for (let i = 0; i < this._orderedMutableNodes.length; i++) {
+      if (this._orderedMutableNodes[i].id !== node.id) continue;
+      index = i;
+      break;
+    }
+
+    if (index === -1) {
+      throw new Error("Node was removed that is not present in the index.");
+    }
+
+    this._orderedMutableNodes.splice(index, 1);
+    // (no need to sort)
+    this.rebuildSecondaryStructures();
+  }
+
+  private rebuildSecondaryStructures() {
+    // update structures
+    this._nodes = [...this._orderedMutableNodes];
+    this._nodeIds = this._orderedMutableNodes.map((node) => node.id);
 
     // notify react stuff
     this.signalAtom.signal(this.jotaiStore.set);
