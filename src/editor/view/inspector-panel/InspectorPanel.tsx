80fcf626b66bd607cc5dd454e731e21632091261
Jirka Mayer <jirkamayer97@gmail.com>
2025-12-08 21:14:21 +0100

    Graph view panel

--- src/editor/view/inspector-panel/InspectorPanel.tsx
+++ src/editor/view/inspector-panel/InspectorPanel.tsx
@@ -1,31 +1,17 @@
 import Sheet from "@mui/joy/Sheet";
-import { useAtomValue } from "jotai";
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionGroup,
-  AccordionSummary,
-  Box,
-  Divider,
-  Typography,
-} from "@mui/joy";
+import { AccordionGroup, Box, Divider, Typography } from "@mui/joy";
 import { SyntaxLinksToolPanel } from "./SyntaxLinksToolPanel";
 import { PrecedenceLinksToolPanel } from "./PrecedenceLinksToolPanel";
-import { useContext } from "react";
-import { EditorContext } from "../../EditorContext";
 import { NodeEditingToolPanel } from "./NodeEditingToolPanel";
 import { TextTranscriptionPanel } from "./TextTranscriptionPanel";
 import { StafflinesToolPanel } from "./StafflinesToolPanel";
+import { SelectionPanel } from "./SelectionPanel";
+import { GraphViewPanel } from "./GraphViewPanel";
 
 /**
  * The right-side panel, showing details about selected nodes.
  */
 export function InspectorPanel() {
-  const { selectionStore } = useContext(EditorContext);
-
-  const selectedNodeIds = useAtomValue(selectionStore.selectedNodeIdsAtom);
-  const selectedNodes = useAtomValue(selectionStore.selectedNodesAtom);
-
   return (
     <Sheet
       variant="outlined"
@@ -51,30 +37,12 @@ export function InspectorPanel() {
       >
         <AccordionGroup>
           <StafflinesToolPanel />
-
           <NodeEditingToolPanel />
-
           <SyntaxLinksToolPanel />
-
           <PrecedenceLinksToolPanel />
-
           <TextTranscriptionPanel />
-
-          <Accordion defaultExpanded={true}>
-            <AccordionSummary>
-              <Typography level="title-sm">Selection</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              selected node IDs: {JSON.stringify(selectedNodeIds)}
-              <pre>
-                {JSON.stringify(
-                  selectedNodes.map((n) => ({ ...n, decodedMask: undefined })),
-                  null,
-                  2,
-                )}
-              </pre>
-            </AccordionDetails>
-          </Accordion>
+          <GraphViewPanel />
+          <SelectionPanel />
         </AccordionGroup>
       </Box>
     </Sheet>
